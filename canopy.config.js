594cffd324a27f48143f211dd205bc45794282b8
Mat Jordan <mat@northwestern.edu>
2023-01-30 16:19:32 -0500

    Update WW2.

--- canopy.config.js
+++ canopy.config.js
@@ -2,30 +2,26 @@
  * Configuration built by `npm run build`
  */
 exports.prod = {
-  label: { none: ["Canopy IIIF"] },
+  label: { none: ["World War II Poster Collection"] },
   collection:
-    "https://api.dc.library.northwestern.edu/api/v2/search?query=%22Edward%20S.%20Curtis%27s%20The%20North%20American%20Indian%20--%20Volume%208.%20The%20Nez%20Perces.%20Wallawalla.%20Umatilla.%20Cayuse.%20The%20Chinookan%20tribes%22&as=iiif&size=500",
+    "https://api.dc.library.northwestern.edu/api/v2/search?query=%22World%20War%20II%20Poster%20Collection%22&as=iiif&size=500",
   featured: [
-    "https://api.dc.library.northwestern.edu/api/v2/works/588a6527-88b6-4b70-ae6c-f42aceea9efb?as=iiif",
-    "https://api.dc.library.northwestern.edu/api/v2/works/14a6aa15-9e12-47a7-9617-91f430d4f47b?as=iiif",
-    "https://api.dc.library.northwestern.edu/api/v2/works/15871197-1616-4c37-9257-b0ad0dc5ab30?as=iiif",
+    "https://api.dc.library.northwestern.edu/api/v2/works/81a7cfb7-5c5e-4b69-9634-17df4aa91980?as=iiif",
   ],
-  metadata: ["Date", "Dimensions", "Genre", "Subject"],
+  metadata: ["Creator", "Date", "Dimensions", "Subject"],
 };
 
 /**
  * Configuration built by `npm run dev`
  */
 exports.dev = {
-  label: { none: ["Canopy IIIF"] },
+  label: { none: ["World War II Poster Collection"] },
   collection:
-    "https://api.dc.library.northwestern.edu/api/v2/search?query=%22Edward%20S.%20Curtis%27s%20The%20North%20American%20Indian%20--%20Volume%208.%20The%20Nez%20Perces.%20Wallawalla.%20Umatilla.%20Cayuse.%20The%20Chinookan%20tribes%22&as=iiif&size=500",
+    "https://api.dc.library.northwestern.edu/api/v2/search?query=%22World%20War%20II%20Poster%20Collection%22&as=iiif&size=500",
   featured: [
-    "https://api.dc.library.northwestern.edu/api/v2/works/588a6527-88b6-4b70-ae6c-f42aceea9efb?as=iiif",
-    "https://api.dc.library.northwestern.edu/api/v2/works/14a6aa15-9e12-47a7-9617-91f430d4f47b?as=iiif",
-    "https://api.dc.library.northwestern.edu/api/v2/works/15871197-1616-4c37-9257-b0ad0dc5ab30?as=iiif",
+    "https://api.dc.library.northwestern.edu/api/v2/works/81a7cfb7-5c5e-4b69-9634-17df4aa91980?as=iiif",
   ],
-  metadata: ["Date", "Dimensions", "Genre", "Subject"],
+  metadata: ["Creator", "Date", "Dimensions", "Subject"],
 };
 
 exports.options = {
